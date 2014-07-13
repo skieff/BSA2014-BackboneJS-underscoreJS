4a3fcb1067b18a2b373926334584704cdaaa548d
Mykyta Semenistyi <nikeiwe@gmail.com>
2014-07-13 17:25:21 +0300

    add help on default route

--- homework/app.js
+++ homework/app.js
@@ -10,5 +10,16 @@ app.get('/api/filmdetails', function (req, res) {
 	res.send(filmService.getFilm(req.query.name));
 });
 
+app.get('/', function(req, res){
+	var text = [
+		'<b>localhost:3000/api/filmlist</b>',
+		'returns the list of films available',
+		'<br />',
+		'<b>localhost:3000/api/filmdetails?name=%filmname%</b>',
+		'where <b>%filmname%</b> is a name of the film from the filmlist'];
+	res.send(text.join('<br />'));
+
+});
+
 app.listen(3000);
 console.log('Server started on port 3000');
