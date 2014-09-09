fee8bbaa0cc0636029e16b9fa0bf8843d39396a9
Sergey <skyeff@gmail.com>
2014-09-10 00:05:13 +0300

    - added: ability to remove film;

--- public/app/javascripts/FilmView.js
+++ public/app/javascripts/FilmView.js
@@ -3,8 +3,27 @@ var FilmView = Backbone.View.extend({
 	template: _.template($('#film-template').html()),
 
 	initialize: function(){
+        this.$el.off('click.film-view').on('click.film-view', '.film-remove', $.proxy(this.onRemoveClick, this));
+        this.listenTo(this.model, 'remove', this.onModelRemove);
 		this.render();
 	},
+
+    onRemoveClick: function() {
+        if (this.model.isNew()) {
+            this.model.collection.remove(this.model);
+        } else {
+            this.model.destroy({
+                success: function(model) {
+                    model.collection.remove(model);
+                }
+            });
+        }
+
+    },
+
+    onModelRemove: function() {
+        this.remove();
+    },
 
 	render: function(){
 		this.$el.html(this.template(this.model.toJSON()));
