e6dfbb457c9f837cf534825f0f80ad1f40e72439
sergeyg <skyeff@gmail.com>
2014-09-15 16:44:28 +0300

    - added: Jade;

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,23 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        jade: {
+            debug: {
+                options: {
+                    pretty: true
+                },
+                files: {
+                    "./public/app/index.html": ["./dev/jade/index.jade"]
+                }
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-jade');
+
     // Default task(s).
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['jade']);
 
 };
