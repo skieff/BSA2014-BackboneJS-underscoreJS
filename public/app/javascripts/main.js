49c17591dcc46ac02a33fd458c7a81e810c1b620
sergeyg <skyeff@gmail.com>
2014-09-19 11:54:49 +0300

    - change: extracted application to separate file;

--- public/app/javascripts/main.js
+++ public/app/javascripts/main.js
@@ -1,40 +1,8 @@
 define(function(require){
-    var Marionette = require("backbone.marionette"),
-        AppRouter = require("Router"),
-        Backbone = require("backbone"),
-        domReady = require("domReady"),
-        FilmCollectionView = require("FilmCollectionView"),
-        FilmCollection = require("FilmCollection"),
-        FullScreenFilmView = require("FullScreenFilmView");
+    var MyApp = require('Application'),
+        domReady = require("domReady");
 
     domReady(function(){
-        var myApp = new Marionette.Application({
-            regions: {
-                content: "#content"
-            }
-        });
-
-        myApp.addInitializer(function(){
-            this.filmCollectionView = new FilmCollectionView({
-                collection: new FilmCollection(),
-                model: new Backbone.Model()
-            });
-
-            new AppRouter();
-            Backbone.history.start();
-        });
-
-        myApp.listenTo(Backbone, 'view:edit-film', function(model) {
-            this.content.show(
-                new FullScreenFilmView({"model": model}),
-                {preventDestroy: (this.content.currentView === this.filmCollectionView)}
-            );
-        });
-
-        myApp.listenTo(Backbone, 'router:view-list', function() {
-            this.content.show(this.filmCollectionView);
-        });
-
-        myApp.start();
+        (new MyApp()).start();
     });
 });
